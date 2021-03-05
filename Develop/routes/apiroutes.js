d6f4e743ba1a6e138131841e62f1b290477633ae
Frank Merchlewitz <jlimhb@gmail.com>
2021-03-04 19:10:10 -0600

    Forgot to hit save

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,9 +1,13 @@
-
+// requiring fs, path (and specifies path location), and uuid.
 const fs = require('fs');
 const path = require('path');
 const dataPath = path.join(_dirname, "../db/dbjson");
 const uuid = require("uuid");
+
+// doing our app export for server routing
+
 module.exports = function (app) {
+    // gets the existing notes from dataPath
     app.get("/api/notes", function (req, res) {
         fs.readFile(dataPath, "utf-8", function (err, data) {
             if (err) {
@@ -15,15 +19,8 @@ module.exports = function (app) {
 
         });
     });
-    
 
-
-    // var newStore = {
-    //     storeID: $("#storeID").val().trim(),
-    //     storeName: $("#storeName").val().trim(),
-    //     storeDescription: $("#storeDescription").val().trim(),
-    //     status: $("#status").val().trim()
-    // };
+   // "Post" takes user entered note data from the page, pushes it to an array of existing notes, and makes the note accessible to the user.
 
     app.post("/api/notes", "/api/notes", function (req, res) {
         fs.readFile(dataPath, "utf-8", function (err, data) {
@@ -34,7 +31,7 @@ module.exports = function (app) {
                 res.json(notes);
             }
                 let createNote = req.body;
-                createNote.id = uuidv4();
+                createNote.id = uuid.v4();
                 notes.push(createNote);
 
                 fs. writeFile(dataPath, JSON.stringify(notes), "utf-8", function (err) {
@@ -45,7 +42,6 @@ module.exports = function (app) {
                         return;
                     }
                 })
-
         });
     });
 
