b499465805efaf5b0121748f49dc8fc1b6e22fa2
Frank Merchlewitz <jlimhb@gmail.com>
2021-03-05 16:55:51 -0600

    Rebuilt post module and stripped conflicting info in delete section

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,65 +1,54 @@
 // requiring fs, path (and specifies path location), and uuid.
 const fs = require('fs');
 const path = require('path');
-const dataPath = path.join(_dirname, "../db/dbjson");
+const dataPath = path.join(__dirname, "../db/db.json");
 const uuid = require("uuid");
+const noteArray = require("../db/db");
 
 // doing our app export for server routing
+
 
 module.exports = function (app) {
     // gets the existing notes from dataPath
     app.get("/api/notes", function (req, res) {
         fs.readFile(dataPath, "utf-8", function (err, data) {
             if (err) {
-                console.log("Sorry, and error occurred. Please try again");
-            } else {
-                var notes = JSON.parse(data);
-                res.json(notes);
+                console.log(err);
             }
+            var notesRes = JSON.parse(data);
+            res.json(notesRes);
 
         });
     });
 
    // "Post" takes user entered note data from the page, pushes it to an array of existing notes, and makes the note accessible to the user.
 
-    app.post("/api/notes", "/api/notes", function (req, res) {
+    app.post("/api/notes", function (req, res) {
         fs.readFile(dataPath, "utf-8", function (err, data) {
+            
             if (err) {
-                console.log("Sorry, and error occurred. Please try again");
-            } else {
-                var notes = JSON.parse(data);
-                res.json(notes);
+                console.log(err)
             }
-                let createNote = req.body;
-                createNote.id = uuid.v4();
-                notes.push(createNote);
+            let notes;
+            if (data) {
+                notes = JSON.parse(data);
+            }
+                
+            let createNote = req.body;
+            createNote.id = uuid.v4();
 
-                fs. writeFile(dataPath, JSON.stringify(notes), "utf-8", function (err) {
-                    if (err) {
-                        console.log("Sorry, an error occurred and your note was not created");
-                    } else {
-                        res.json(notes);
-                        return;
-                    }
-                })
+            if (data) {
+            notes.push(createNote);
+            } else {
+                notes = [createNote];
+            }
+
+            fs. writeFile(dataPath, JSON.stringify(notes), "utf-8", function (err) {
+                if (err) throw err;
+                res.json(createNote);
+                
+            })
         });
     });
 
-    // delete functionality using the splice method. Credit to Lita Beach who shared this solution option in the Bootcamp Slack channel
-    app.delete("/api/notes/:id", function (req, res) {
-        // we want to take the id of the note we want to delete
-        var noteId = req.params.id
-        // looping through to find a matching note id
-        for (i = 0; i < notes.length; i++){
-            if (notes[i].id === noteId) {
-                notes.splice(i, 1);
-            }
-        }
-        // updating the main note filesystem and returning it to user
-        fs.writeFileSync(path.join(__dirname, "../db/dbjson"), JSON.stringify(notes));
-
-        res.json(req.body);
-
-    });
-};
-
+}
